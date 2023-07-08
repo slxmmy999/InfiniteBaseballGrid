0c74679e6d02da4f183ff910517175d704d48f7b
Sam Coan <62761327+slxmmy999@users.noreply.github.com>
2023-07-08 17:17:36 -0400

    initial commit

---
+++ components/game-grid.vue
@@ -0,0 +1,45 @@
+<template>
+    <div :class="$style.grid">
+        <button :class="$style['grid-item']"></button>
+        <button :class="$style['grid-item']"></button>
+        <button :class="$style['grid-item']"></button>
+        <button :class="$style['grid-item']"></button>
+        <button :class="$style['grid-item']"></button>
+        <button :class="$style['grid-item']"></button>
+        <button :class="$style['grid-item']"></button>
+        <button :class="$style['grid-item']"></button>
+        <button :class="$style['grid-item']"></button>
+    </div>
+</template>
+
+<style module>
+    .grid {
+        width: 25%;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 1px; /* adjust as needed */
+        border-radius: 15px;
+        margin: auto;
+        padding-top: 10%;
+    }
+
+    .grid-item {
+        aspect-ratio: 1 / 1;
+        background-color: #383842; /* adjust as needed */
+        color: white; /* adjust as needed */
+        border: none; /* adjust as needed */
+        padding: 10px; /* adjust as needed */
+        font-size: 1em; /* adjust as needed */
+        cursor: pointer;
+    }
+
+    .grid-item:hover {
+        background-color: #565666; /* adjust as needed */
+    }
+</style>
+
+<script>
+export default {
+  name: 'game-grid'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <game-grid :class="$style.grid" />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style module>
+  .grid {
+    align-items: center;
+  }
+</style>
