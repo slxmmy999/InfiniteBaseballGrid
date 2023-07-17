af60e228003c0d68742cb2d039c90b6df81a77e0
Sam Coan <62761327+slxmmy999@users.noreply.github.com>
2023-07-17 18:31:05 -0400

    added analytics?

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,6 +32,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/vercel.js', mode: 'client'}
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
