bc8bf1010e8082d420565db308af5ef6cac7f7d6
Sam Coan <62761327+slxmmy999@users.noreply.github.com>
2023-07-17 19:07:49 -0400

    switched server to quart

--- server/server.py
+++ server/server.py
@@ -1,21 +1,21 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from quart import Quart, request, jsonify
+from quart_cors import cors
 from server.GameCategories import GameCategories
 from server.BaseballData import BaseballData
 
-app = Flask(__name__)
+app = Quart(__name__)
 
-CORS(app, origins=["https://www.infiniteimmaculategrid.com"])
+app = cors(app, origins=["https://www.infiniteimmaculategrid.com"])
 
 @app.route("/get_new_grid", methods=["GET"])
-def get_new_grid():
+async def get_new_grid():
     categories = GameCategories()
     return jsonify(categories.get_grid())
 
 @app.route("/search_players", methods=["GET"])
-def search_players():
+async def search_players():
     query = request.args.get("name")
-    players = BaseballData.search_players(query)
+    players = await BaseballData.search_players(query)
     data = players
     if len(data) > 0:
         if len(data) > 5:
@@ -27,15 +27,15 @@ def search_players():
     return jsonify([])
 
 @app.route("/validate_player", methods=["GET"])
-def validate_player():
+async def validate_player():
     query = request.args.get("name")
     team1 = request.args.get("team1")
     team2 = request.args.get("team2")
-    players = BaseballData.search_players(query)
-    teams = BaseballData.get_player_teams(players[0])
+    players = await BaseballData.search_players(query)
+    teams = await BaseballData.get_player_teams(players[0])
     if teams:
         if team1 in teams and team2 in teams:
-            picture = BaseballData.get_player_picture(players[0])
+            picture = await BaseballData.get_player_picture(players[0])
             name = players[0]["fullName"]
             return jsonify({"picture": picture, "name": name})
     # return nothing
