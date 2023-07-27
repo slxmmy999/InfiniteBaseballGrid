32a92b94ed7d55bf408cb41cade36b13807978c7
Sam Coan <62761327+slxmmy999@users.noreply.github.com>
2023-07-26 20:42:22 -0400

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,8 +37,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/vercel.js', mode: 'client'},
-    { src: '~/plugins/adsense.js', mode: 'client' }
+    { src: '~/plugins/vercel.js', mode: 'client'}
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
--- plugins/adsense.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import Ads from 'vue-adsense'
-
-Vue.component('Ads', Ads)
