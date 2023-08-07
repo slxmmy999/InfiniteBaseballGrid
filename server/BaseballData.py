65f56d0b8f6f50b7a0d160a48ca8edc9c67a2f0b
C. J. Meidlinger <cj@meidlinger.org>
2023-08-07 16:48:20 -0400

    more old team names with some notes

--- server/BaseballData.py
+++ server/BaseballData.py
@@ -38,11 +38,19 @@ class BaseballData:
     @staticmethod    
     def __get_old_team_names(team):
         match(team):
+            case "Cleveland Blues":
+                return "Cleveland Guardians"
+            case "Cleveland Bronchos":
+                return "Cleveland Guardians"
+            case "Cleveland Naps":
+                return "Cleveland Guardians"
             case "Cleveland Indians":
                 return "Cleveland Guardians"
             case "California Angels":
                 return "Los Angeles Angels"
             case "Anaheim Angels":
+                return "Los Angeles Angels"
+            case "Los Angeles Angels of Anaheim":
                 return "Los Angeles Angels"
             case "Florida Marlins":
                 return "Miami Marlins"
@@ -50,16 +58,55 @@ class BaseballData:
                 return "Tampa Bay Rays"
             case "Montreal Expos":
                 return "Washington Nationals"
+            # 1901-1960 Washington Senators became Minnesota Twins. 1961-1971 Washington Senators became current Rangers.
             case "Washington Senators":
                 return "Minnesota Twins"
+            # Milwaukee Brewers 1901-1901 are also part of the current Orioles franchise.
+            # 1883-1898 St. Louis Browns became St. Louis Cardinals.  1902-1953 St. Louis Browns became Baltimore Orioles.
             case "St. Louis Browns":
                 return "Baltimore Orioles"
+            case "St. Louis Brown Stockings":
+                return "St. Louis Cardinals"
+            case "St. Louis Perfectos":
+                return "St. Louis Cardinals"
+            case "Cincinnati Red Stockings":
+                return "Cincinnati Reds"
+            case "Cincinnati Redlegs":
+                return "Cincinnati Reds"
+            case "Boston Red Stockings":
+                return "Atlanta Braves"
+            case "Boston Beaneaters":
+                return "Atlanta Braves"
+            case "Boston Doves":
+                return "Atlanta Braves"
+            case "Boston Rustlers":
+                return "Atlanta Braves"
+            case "Boston Bees":
+                return "Atlanta Braves"
             case "Boston Braves":
                 return "Atlanta Braves"
             case "Milwaukee Braves":
                 return "Atlanta Braves"
+            case "Brooklyn Atlantics":
+                return "Los Angeles Dodgers"
+            case "Brooklyn Grays":
+                return "Los Angeles Dodgers"
+            case "Brooklyn Grooms":
+                return "Los Angeles Dodgers"
+            # Bridegrooms overlapped with Grooms?
+            # https://www.baseball-reference.com/teams/LAD/
+            case "Brooklyn Bridegrooms":
+                return "Los Angeles Dodgers"
+            # Superbas and Robins overlapped with Dodgers?
+            # https://www.baseball-reference.com/teams/LAD/
+            case "Brooklyn Superbas":
+                return "Los Angeles Dodgers"
+            case "Brooklyn Robins":
+                return "Los Angeles Dodgers"
             case "Brooklyn Dodgers":
                 return "Los Angeles Dodgers"
+            case "New York Gothams":
+                return "San Francisco Giants"
             case "New York Giants":
                 return "San Francisco Giants"
             case "New York Highlanders":
@@ -68,6 +115,22 @@ class BaseballData:
                 return "Oakland Athletics"
             case "Kansas City Athletics":
                 return "Oakland Athletics"
+            case "Boston Americans":
+                return "Boston Red Sox"
+            case "Houston Colt .45s":
+                return "Houston Astros"
+            case "Philadelphia Quakers":
+                return "Philadelphia Phillies"
+            case "Seattle Pilots":
+                return "Milwaukee Brewers"
+            case "Chicago White Stockings":
+                return "Chicago Cubs"
+            case "Chicago Colts":
+                return "Chicago Cubs"
+            case "Chicago Orphans":
+                return "Chicago Cubs"
+            case "Pittsburgh Alleghenys":
+                return "Pittsburgh Pirates"
             case _:
                 return team
     
