2e46bc20c942d169b2098fbd24eeb2ca156878a7
Sam Coan <62761327+slxmmy999@users.noreply.github.com>
2023-08-28 01:58:10 -0600

    Update server.py

--- server/server.py
+++ server/server.py
@@ -20,7 +20,7 @@ else:
 import datetime
 
 mongo_client = AsyncIOMotorClient(database_connection_string)
-db: Database = Database(mongo_client, False)
+db: Database = Database(mongo_client, dev)
 
 app = Quart(__name__)
 
