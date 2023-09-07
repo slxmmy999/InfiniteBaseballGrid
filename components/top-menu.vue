0b896e8c839c08c570bca6234a89a4e78fde8550
Sam Coan <62761327+slxmmy999@users.noreply.github.com>
2023-09-06 19:19:04 -0600

    Update top-menu.vue

--- components/top-menu.vue
+++ components/top-menu.vue
@@ -1,22 +1,31 @@
 <template>
-    <div class="container">
-        <svg class="logo-svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-            <path d="M16 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zm13 0a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zm13 0a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zM16 27a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zm13 0a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zm13 0a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zM16 40a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zm13 0a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zm13 0a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2z" fill="#59d185"></path>
-        </svg>
-        <h2 class="title">INFINITE BASEBALL GRID</h2>
-        <div class="social-icons">
-            <a href="https://twitter.com/sam_coan">
-                <img src="~/static/twitter.svg" alt="Twitter Logo" />
-            </a>
-            <a href="https://github.com/slxmmy999/InfiniteImmaculateGrid">
-                <img src="~/static/github.svg" alt="Github Logo" />
-            </a>
-            <a href="https://discord.gg/rPBuvhqG8p">
-                <img src="~/static/discord.svg" alt="Discord" />
-            </a>
-            <a href="https://allmylinks.com/sam-coan">
-                <img src="~/static/donate.png" alt="Donation Logo" />
-            </a>
+    <div class="main">
+        <div class="container">
+            <svg class="logo-svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <path d="M16 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zm13 0a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zm13 0a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zM16 27a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zm13 0a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zm13 0a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zM16 40a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zm13 0a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2zm13 0a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2z" fill="#59d185"></path>
+            </svg>
+            <h2 class="title">INFINITE BASEBALL GRID</h2>
+            <div class="social-icons">
+                <a href="https://twitter.com/sam_coan">
+                    <img src="~/static/twitter.svg" alt="Twitter Logo" />
+                </a>
+                <a href="https://github.com/slxmmy999/InfiniteImmaculateGrid">
+                    <img src="~/static/github.svg" alt="Github Logo" />
+                </a>
+                <a href="https://discord.gg/rPBuvhqG8p">
+                    <img src="~/static/discord.svg" alt="Discord" />
+                </a>
+                <a href="https://allmylinks.com/sam-coan">
+                    <img src="~/static/donate.png" alt="Donation Logo" />
+                </a>
+                <a class="donatelolz" @click="showDonateMenu = !showDonateMenu">
+                    <button>Donate $0.69</button>
+                </a>
+            </div>
+        </div>
+        <div v-if="showDonateMenu" class="donate69">
+            <button class="venmo" @click="venmo">Venmo</button>
+            <button @click="paypal">PayPal</button>
         </div>
     </div>
 </template>
@@ -32,12 +41,22 @@
 
         .social-icons {
             flex-direction: column !important;
+            align-items: last baseline !important;
         }
 
         .social-icons a {
             margin-right: 0% !important;
             margin-bottom: 20% !important;
         }
+
+        .donatelolz {
+            width: 50% !important;
+        }
+    }
+    .main {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
     }
     .logo-svg {
         width: 100px; /* Adjust to your preference */
@@ -74,10 +93,83 @@
         width: 30px; /* Adjust to your preference */
         height: 30px; /* Adjust to your preference */
     }
+
+    .donatelolz {
+        margin-right: 5% !important;
+        margin-bottom: 20% !important;
+    }
+
+    .donatelolz button {
+        background-color: #141421;
+        color: white;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 700;
+        margin: 0px;
+        margin-top: 0%;
+        border-radius: 5px;
+        border-width: 0;
+    }
+
+    .donatelolz button:hover {
+        background-color: #21213b;
+        color: white;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 700;
+        margin: 0px;
+        margin-top: 0%;
+        border-radius: 5px;
+        border-width: 0;
+    }
+
+    .donate69 {
+        display: flex;
+        align-items: flex-end;
+        flex-direction: column;
+        margin-left: auto;
+        width: auto;
+        padding: 0.2%;
+        margin-top: 0%;
+        margin-right: 0.7%;
+        background-color: #2b2b46;
+    }
+
+    .donate69 button {
+        background-color: transparent;
+        width: fit-content;
+        padding: 0%;
+        color: white;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 700;
+        border-radius: 5px;
+        border-width: 0;
+    }
+
+    .venmo {
+        margin-bottom: 70% !important;
+    }
+
+    .donate69 button:hover {
+        background-color: #21213b;
+        color: white;
+        font-family: 'Roboto', sans-serif;
+    }
 </style>
 
 <script>
 export default {
-  name: 'top-menu'
+  name: 'top-menu',
+  data () {
+    return {
+      showDonateMenu: false
+    }
+  },
+  methods: {
+    venmo () {
+      window.open('https://account.venmo.com/payment-link?audience=private&amount=0.69&recipients=Sam-Coan-1&txn=pay', '_blank')
+    },
+    paypal () {
+      window.open('https://www.paypal.com/paypalme/samcoan4/0.69', '_blank')
+    }
+  }
 }
 </script>
